5d5c7a5190aac20ff37d679bd5655591a8e8bb13
uepzues <kimsanz@gmail.com>
2025-02-13 11:52:07 +0800

    fixed relative path

--- src/assets/scripts/background.js
+++ src/assets/scripts/background.js
@@ -4,17 +4,17 @@ const location = document.URL.split('/').pop()
 
 switch (location) {
   case '':
-    body.style.backgroundImage = `url(src/assets/images/home/background-home-desktop.jpg)`
+    body.style.backgroundImage = `url(/src/assets/images/home/background-home-desktop.jpg)`
     break
   case 'Destination':
-    body.style.backgroundImage = `url(src/assets/images/destination/background-destination-desktop.jpg)`
+    body.style.backgroundImage = `url(/src/assets/images/destination/background-destination-desktop.jpg)`
     break
   case 'Crew':
-    body.style.backgroundImage = `url(src/assets/images/crew/background-crew-desktop.jpg)`
+    body.style.backgroundImage = `url(/src/assets/images/crew/background-crew-desktop.jpg)`
     break
   case 'Technology':
-    body.style.backgroundImage = `url(src/assets/images/technology/background-technology-desktop.jpg)`
+    body.style.backgroundImage = `url(/src/assets/images/technology/background-technology-desktop.jpg)`
     break
   default:
-    body.style.backgroundImage = `urlsrc/assets/images/home/background-home-desktop.jpg)`
+    body.style.backgroundImage = `url(/src/assets/images/home/background-home-desktop.jpg)`
 }
